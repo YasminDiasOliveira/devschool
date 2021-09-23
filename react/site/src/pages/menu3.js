a2666986fbd291d84122484542d2fc432a1ec552
Yasmin <89045311+YasminDiasOliveira@users.noreply.github.com>
2021-09-23 14:56:40 -0300

    Update menu3.js

--- react/site/src/pages/menu3.js
+++ react/site/src/pages/menu3.js
@@ -2,14 +2,13 @@ import { Menu } from "../components/menu";
 import { Faixa } from "../components/faixa";
 import { Rosa } from "../components/rosa";
 import { Menu2 } from "../components/menu2";
-import { Fonte_Rosa } from "../components/fonte-rosa";
 
 export default function Menu3() {
   return (
     <Menu>
       <div class="logo">
         <img src="/imgs/logo.png" alt="" />
-        <Fonte_Rosa>Dev</Fonte_Rosa>School
+         DevSchool
       </div>
 
       <Faixa />
